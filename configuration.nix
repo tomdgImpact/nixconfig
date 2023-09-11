4430693a6a0d91ed79c692bfc9ffcb117e30431c
TomDG <58532256+tomdgImpact@users.noreply.github.com>
2023-09-12 00:21:16 +0200

    update conf nix for new pc

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, callPackage ,... }:
+{ config, pkgs, callPackage, ... }:
 
 {
   imports =
@@ -11,15 +11,12 @@
     ];
 
   # Bootloader.
-  #boot.loader.systemd-boot.enable = true;
-  #boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.enable = true;
-  boot.loader.grub.device = "nodev";
+  boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
 
-  # Networking capabilities
   networking.hostName = "nixos"; # Define your hostname.
-  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
@@ -45,73 +42,68 @@
     LC_TELEPHONE = "fr_FR.UTF-8";
     LC_TIME = "fr_FR.UTF-8";
   };
-
-  # Enable the XFCE Desktop Environment.
-  #services.xserver.displayManager.lightdm.enable = true;
-  #services.xserver.desktopManager.xfce.enable = true;
-
-  environment.pathsToLink = [ "/libexec" ]; # links /libexec from derivations to /run/current-system/sw 
-
-  # i3 config for nixOS
-   services.xserver = {
-    enable = true;
+  environment.pathsToLink = ["/libexec"];
+  # Configure keymap in X11
+  services.xserver = {
     layout = "fr";
     xkbVariant = "";
-    desktopManager = {
-      xterm.enable = false;
-    };
+    enable = true;
    
+    desktopManager = {
+	xterm.enable = false;
+	wallpaper = {
+		combineScreens = false;
+		mode = "scale";
+	};
+    };
     displayManager = {
-        defaultSession = "none+i3";
+	defaultSession = "none+i3";
     };
 
     windowManager.i3 = {
-      enable = true;
-      extraPackages = with pkgs; [
-        dmenu #application launcher most people use
-        i3status # gives you the default i3 status bar
-        i3lock #default i3 screen locker
-        i3blocks #if you are planning on using i3blocks over i3status
-     ];
+	enable = true;
+	extraPackages = with pkgs; [
+	   dmenu
+	   i3status
+	   i3lock
+	];
     };
-  }; 
-
+  };
+  nixpkgs.config.pulseaudio = true;
   # Configure console keymap
   console.keyMap = "fr";
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  #services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.impactsec = {
     isNormalUser = true;
     description = "Impactsec";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      firefox
-    #  thunderbird
-    ];
+    extraGroups = [ "networkmanager" "wheel" "docker" "libvirtd" ];
+    packages = with pkgs; [];
   };
+  # Bluetooth setting and sound
+  sound.enable = true;
+  hardware = {
+	pulseaudio = {
+		enable = true;
+		package = pkgs.pulseaudioFull;
+	};
+	bluetooth = {
+		enable = true;
+		settings = {
+			General = {
+				Enable = "Source, Sink, Media, Socket";
+			};
+		};
+	};
+	ckb-next.enable = true;
+	opengl = {
+		enable = true;
+		driSupport32Bit = true;
+		setLdLibraryPath = true;
+	};
+  };
+  services.blueman.enable = true;
+  services.mmsd.enable = true;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
@@ -121,21 +113,124 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
+    bluez
+    git
     brave
     discord
+    firefox
+    polybar
+    python311
+    python311Packages.pip
+    python311Packages.jupyterlab
+    ansible
+    jupyter
+    gcc
+    gdb
     docker
     kubectl
-    terraform
-    polybar  
-    vscode
+    terraform  
+    xfce.thunar
+    gparted
+    nitrogen
+    clipit
+    flameshot
+    pavucontrol
     jetbrains-toolbox
+    vscode
+    nodejs_20
+    rustup
+    ghidra-bin
+    teams
+    qbittorrent
     oh-my-zsh
-    git
     zsh-powerlevel10k
-    gdb
-    libgccjit
+    tmux
+    xterm
+    bat
+    rofi
+    htop
+    podman
+    cmake
+    clang
+    clang-tools
+    arandr
+    rust-analyzer
+    neofetch
+    dunst
+    usbutils
+    # synthing  # package for file synchro, homemade cloud storage ?
+    xorg.xkill
+    killall
+    dnsmasq
+    # tailscale # mesh vpn built on wireguard 
+    opensnitch-ui
+    openssl
+    ngrok
+    unzip
+    feh
+    gnumake
+    xclip
+    pkgconfig
+    podman
+    noisetorch
+    # package to handle virtualization
+    virt-manager
+    libvirt
+    qemu
+    virtualbox
+    dconf
+    alacritty
+    clightd
+    lm_sensors
+    redshift
+    cpuid
+    mpd
+    xfce.xfce4-battery-plugin
+    xbattbar
+    xorg.xbacklight
+  ];
+  
+  # set zsh as default shell
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
+  environment.shells = with pkgs; [ zsh ];
+  fonts.fonts = with pkgs; [
+	(nerdfonts.override { fonts = [ "UbuntuMono" ]; })
   ];
 
+  programs.noisetorch.enable = true;
+  programs.dconf.enable = true;
+  # polkit
+  security.polkit.enable = true; 
+  # vm setup config
+  virtualisation.libvirtd = {
+	enable = true;
+	onBoot = "ignore";
+	onShutdown = "shutdown";
+	qemu.ovmf.enable = true;
+	qemu.runAsRoot = true;
+  };
+
+  # virtualization with container 
+  virtualisation = {
+	docker = {
+		enable = true;
+		rootless = {
+			enable = true;
+			setSocketVariable = true;
+		};
+	};
+        podman = {
+		enable = true;
+		# Create a 'docker' alias for podman, to use it as a drop-in replacement
+		# dockerCompat = true;
+		
+		# Required for containers under podman-compose to be able to talk to each other.
+		defaultNetwork.settings = {
+			dns_enable = true;
+		};
+	};
+  }; 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -147,13 +242,13 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  #services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
